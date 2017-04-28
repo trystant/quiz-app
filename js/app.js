653330235dfb48f40c4d3f7b6b6b112fd4936e24
hentschellj@gmail.com <hentschellj@gmail.com>
2017-04-27 21:06:33 -0500

    Minor fix in answer header

--- js/app.js
+++ js/app.js
@@ -169,6 +169,7 @@ function renderQuestionChoices(state, element) {
 
 function renderAnswerHeader(state, element) {
 	var html = state.lastAnswer ? '<h3 class="correct-answer">Correct!</h3>' : '<h3 class="incorrect-answer">Incorrect!</h3>';
+	element.html(html);
 };
 
 function renderAnswerText(state, element) {
